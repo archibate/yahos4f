9d49e4b6eecd67653cf335b31076719dc4592589
archibate <m17721388340@163.com>
2019-07-24 14:38:22 +0800

    stage 04-1

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include "console.h"
 #include "gdt.h"
+#include "idt.h"
 
 void main(void)
 {
 	puts("Kernel Started...\n");
 	init_gdt();
 	puts("\nGDT Initialized!\n");
+	init_idt();
+	puts("\nIDT Initialized!\n");
+	puts("\nDoing 1/0 Test...");
+	volatile int x = 0;
+	volatile int y = 1 / x;
+	puts("\n1/0 Test Done (should not be)\n");
 }
