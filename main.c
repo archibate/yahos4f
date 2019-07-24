8ead675ce4e89df8b115d5eb5bab7b6896c319ab
archibate <m17721388340@163.com>
2019-07-24 13:44:12 +0800

    stage 02-3

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 int putchar(int c)
 {
-	static short *vram = (short *)0xb8000;
-	*vram++ = 0xf00 + c;
+	short *const vram = (short *)0xb8000;
+	static int x, y;
+	const int nx = 80, ny = 25;
+
+	if (c == '\n') {
+		x = 0;
+		y++;
+	} else {
+		vram[y * nx + x] = 0xf00 + c;
+		x++;
+	}
 }
 
 int puts(const char *s)
@@ -12,5 +21,6 @@ int puts(const char *s)
 
 void main(void)
 {
-	puts("Hello, World!");
+	puts("Hello, World!\nThis is my Very First OS!\n");
+	puts("\nJust do It!\n");
 }
